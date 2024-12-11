c5658e9e9e204285d3ecb7b18a480ac9701275c9
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-11 13:32:20 +0530

    Add files via upload

---
+++ publicam.java
@@ -0,0 +1,12 @@
+class publicam
+{
+    public static void s()
+    {
+        System.out.println("hello students");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+
+}
