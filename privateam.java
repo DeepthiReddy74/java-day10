4069a0f429675571c71f73ebe58e5f82dce7427d
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-11 13:31:38 +0530

    Add files via upload

---
+++ privateam.java
@@ -0,0 +1,12 @@
+class privateam
+{
+    private static void s()
+    {
+        System.out.println("hello students");
+    }
+    public static void main(String args[])
+    {
+        s();
+    }
+
+}
