c5e8056e77434279b73acb0b615e4d74f462df5d
DeepthiReddy74 <reddydeepthi68699@gmail.com>
2024-12-11 13:33:08 +0530

    Add files via upload

---
+++ staticnonvoid.java
@@ -0,0 +1,13 @@
+class staticnonvoid
+{
+    static String s()
+    {
+        return "hello student";
+
+    }
+    public static void main(String args[])
+    {
+        String res=s();
+        System.out.println(res);
+    }
+}
